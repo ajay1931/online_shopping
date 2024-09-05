4974ea26319a11e66c542a1f45349c4b220dab02
ajay1931 <ajayprakash032@gmail.com>
2024-09-05 14:01:24 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/pages/homepage/Home';
+import SearchPage from './components/pages/searchpage/SearchPage';
+import Products from './components/products/Products';
+import ProductDetails from './components/productdetails/ProductDetails';
+import Cart from './components/cart/Cart';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
+import supabase from './supabase';
+import { useEffect } from 'react';
+import { setUser } from './components/slices/userSlice';
 
 function App() {
+
+const dispatch=useDispatch();
+
+const getUser= async()=>{
+  const {data,error} = await supabase.auth.getSession();
+  if(data.session){
+    dispatch(setUser(data.session.user));
+  }
+}
+
+useEffect(()=>{
+  getUser();
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/searchpage' element={<SearchPage />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/productdetails/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
